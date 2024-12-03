6e16a169cea6e717efa40c31936a44ec0701fe12
Carlo Echeverría <cecheverria@realmedianetwork.com>
2024-12-03 16:10:58 -0300

    Update gatsbyjs gatsby-config.js

--- gatsbyjs/gatsby-config.js
+++ gatsbyjs/gatsby-config.js
@@ -1,5 +1,5 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`, // Carga el archivo según el entorno (ejemplo: .env.development o .env.production)
+require('dotenv').config({
+  path: `.env.local`, // Carga .env.development o .env.production según NODE_ENV
 });
 
 /**
@@ -15,10 +15,8 @@ module.exports = {
     {
       resolve: "gatsby-source-drupal",
       options: {
-        options: {
-          baseUrl: process.env.GATSBY_DRUPAL_BASE_URL,
-          apiBase: process.env.GATSBY_API_BASE,
-        },
+        baseUrl: process.env.GATSBY_DRUPAL_BASE_URL,
+        apiBase: process.env.GATSBY_API_BASE,
       },
     },
   ]
