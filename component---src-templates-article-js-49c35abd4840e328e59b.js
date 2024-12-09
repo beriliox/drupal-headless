ddd3d5aa688e0093f22d8e4cac9073d09c795600
beriliox <beriliox@users.noreply.github.com>
2024-12-09 00:17:47 +0000

    deploy: f3846100147ac4fb5980b6e9f4a9e61066daf8e6

--- component---src-templates-article-js-49c35abd4840e328e59b.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkdrupal_headless_blog=self.webpackChunkdrupal_headless_blog||[]).push([[886],{263:function(e,l,t){t.r(l);var n=t(7294);l.default=e=>{let{pageContext:l}=e;const{title:t,body:a,tags:r}=l;return n.createElement("div",null,n.createElement("h1",null,t),n.createElement("div",{dangerouslySetInnerHTML:{__html:a.processed}}),n.createElement("ul",null,r.map(((e,l)=>n.createElement("li",{key:l},e.name)))))}}}]);
-//# sourceMappingURL=component---src-templates-article-js-49c35abd4840e328e59b.js.map
